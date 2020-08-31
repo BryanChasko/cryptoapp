be25609b8408b7bcb877f0f6b95cc94779ea4ff4
Bryan J Chasko <bryan@bryans-air.lan>
2020-08-31 14:05:50 -0600

    Joseph McBride showed me some ways to place the code in accordancd with react best practices for clean code and we used the Axios library to call on the coinlore API and created react buttons for a dynamic app with user input

--- amplify/backend/function/cryptofunction/src/app.js
+++ amplify/backend/function/cryptofunction/src/app.js
@@ -5,10 +5,13 @@ Licensed under the Apache License, Version 2.0 (the "License"). You may not use 
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
-
+//added this line with joe to try and triage error CORS no access
 var express = require("express");
 var bodyParser = require("body-parser");
 var awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
+
+// Import axios this is the second step weve taken in this file from nader dabits serverless book
+const axios = require("axios");
 
 // declare a new express app
 var app = express();
@@ -25,17 +28,26 @@ app.use(function (req, res, next) {
   next();
 });
 
-/* amplify/backend/function/cryptofunction/src/app.js this is from Nader Dabits serverless book ch 2 */
-
 app.get("/coins", function (req, res) {
-  const coins = [
-    { name: "Bitcoin", symbol: "BTC", price_usd: "10000" },
-    { name: "Ethereum", symbol: "ETH", price_usd: "400" },
-    { name: "Litecoin", symbol: "LTC", price_usd: "150" },
-  ];
-  res.json({
-    coins,
-  });
+  // Define base url
+  let apiUrl = `https://api.coinlore.com/api/tickers?start=0&limit=10`;
+  // Check if there are any query string parameters
+  // If so, reset the base url to include them
+  if (req.apiGateway && req.apiGateway.event.queryStringParameters) {
+    const {
+      start = 0,
+      limit = 10,
+    } = req.apiGateway.event.queryStringParameters;
+    apiUrl = `https://api.coinlore.com/api/tickers/?start=${start}&limit=${limit}`;
+  }
+
+  // Call API and return response
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      res.json({ coins: response.data.data });
+    })
+    .catch((err) => res.json({ error: err }));
 });
 
 /**********************
--- src/App.js
+++ src/App.js
@@ -1,40 +1,65 @@
-import Amplify from 'aws-amplify'
-import config from './aws-exports'
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
 // Import useState and useEffect hooks from React
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 // Import the API category from AWS Amplify
-import { API } from 'aws-amplify'
-import './App.css';
+import { API } from "aws-amplify";
+import "./App.css";
 
-
-Amplify.configure(config) 
+Amplify.configure(config);
 
 function App() {
   // Create coins variable and set to empty array
-  const [coins, updateCoins] = useState([])
+  const [coins, updateCoins] = useState([]);
   // Define function to all API
 
+  // Create additional state to hold user input for limit and start properties
+  const [input, updateInput] = useState({ limit: 5, start: 0 });
+  // Create a new function to allow users to update the input values
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value });
+  }
+  // Update fetchCoins function to use limit and start properties
   async function fetchCoins() {
-      const data = await API.get('cryptoapi', '/coins')
-      updateCoins(data.coins)
-    }
-    // Call fetchCoins function when component loads
-    useEffect(() => {
-      fetchCoins()
-    }, [])
-    return (
-      <div className="App">
-        {
-        coins.map((coin, index) => (
-          <div key={index}>
-            <h2>{coin.name} {coin.symbol}</h2>
-            <h5>${coin.price_usd}</h5>
-          </div>
-      ))
-    }
-      </div>
+    const { limit, start } = input;
+    const data = await API.get(
+      "cryptoapi",
+      `/coins?limit=${limit}&start=${start}`
+    );
+    updateCoins(data.coins);
+  }
+
+  // Call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins();
+  }, [fetchCoins]);
+  return (
+    <div className="App">
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h2>
+            {coin.name} {coin.symbol}
+          </h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
+      {
+        //Add input fields to the UI for user input
+      }
+      <input
+        onChange={(e) => updateInputValues("limit", e.target.value)}
+        placeholder="limit"
+      />
+      <input
+        placeholder="start"
+        onChange={(e) => updateInputValues("start", e.target.value)}
+      />
+      {
+        // Add button to the UI to give user the option to call the API
+      }
+      <button onClick={fetchCoins}>Fetch Coins</button>
+    </div>
   );
 }
-
 
 export default App;
